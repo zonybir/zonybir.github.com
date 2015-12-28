8d884b77638b7341aa773bb433a521e342bfb37a
zonybir-home <zonybir@icloud.com>
2015-12-28 21:11:54 +0800

    change animate and with object

--- public/js/persion.js
+++ public/js/persion.js
@@ -1,27 +1,59 @@
 window.onload=function(){
-	var cri=document.createElement('div');
-	$(cri).css({
-		width:'80px',
-		height:'80px',
-		border:'2px solid #0f0',
-		borderRadius:'50%',
-		position:'absolute',
-		top:30+'px',
-		left:'100px'
-	})
-	$('body')[0].appendChild(cri);
-	var winH=document.body.clientHeight || document.body.offsetHeight || window.screen.availHeight,
-	winW=	document.body.clientWidth || document.body.offsetWidth || window.screen.availWidth;
-	console.log(winH+'-----'+winW);
-	var top=0;
-	function animate(){
-		top=parseInt(cri.style.top);
-		if(top >= winH){
-			cri.style.top=winH-90+'px';
-			return;
+	var d={
+		single_cir:'',
+		WH:0,
+		WH:0,
+		item:[],
+		v:5,
+		init:function(){
+			var cir=document.createElement('div');
+			this.WH=document.body.clientHeight || document.body.offsetHeight || window.screen.availHeight;
+			this.WW=document.body.clientWidth || document.body.offsetWidth || window.screen.availWidth;
+			$(cir).css({
+				width:'80px',
+				height:'80px',
+				border:'2px solid #0f0',
+				borderRadius:'50%',
+				position:'absolute',
+				top:30+'px',
+				left:'100px'
+			})
+			this.single_cir=cir;
+			
+			return this;
+		},
+		cri_factory:function(num){
+			for (var i=0;i<num;i++)	this.item.push(this.single_cir);
+			return this;
+		},
+		animate:function(){
+			var top=0,t=this,h=t.WH-80,w=t.WW-80,v=t.v,ele=t.item[0];
+
+			function main(){
+				top=parseInt(ele.style.top,10);
+				top >= h ?
+				(function(){
+					v=-v;
+					return top;
+				}()) :
+				top <= 0 ? 
+				(function(){
+					v=-v;
+					return top;
+				}()) : '';
+				ele.style.top=top + v +'px';
+				console.log(top);
+				setTimeout(main,40);
+			}
+			main();
+		},
+		start:function(num){
+			this.init();
+			if(!num) num=1;
+			this.cri_factory(num);
+			$('body')[0].appendChild(this.item[0]);
+			this.animate();
 		}
-		cri.style.top=top+100+'px';
-		setTimeout(animate,50);
 	}
-	animate();
+	d.start(1);
 }
